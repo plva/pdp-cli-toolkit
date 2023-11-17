6bbc8b31a8a076b5bef9311ebd3ad48319775f68
Paul V <paul.vasiu@gmail.com>
2023-11-17 11:41:01 -0500

    Ignore git and node_modules in fzfn

---
+++ scripts/fzf-helpers.sh
@@ -0,0 +1,7 @@
+# DOCUMENTATION_START
+#
+# - Helpers for fzf utility, including configuration for env vars
+#
+# DOCUMENTATION_END
+
+export FZF_DEFAULT_COMMAND='find . \( -name node_modules -o -name .git \) -prune -o -print'
